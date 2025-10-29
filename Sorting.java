cd3e3875f8ca9f190e0bd9123f38d8f2063597e4
Shruti <shrutidutta8403@gmail.com>
2025-10-30 01:25:12 +0530

    feat: add sorting algorithms (bubble, selection, insertion, merge, quick)

---
+++ Sorting.java
@@ -0,0 +1,134 @@
+public class Sorting {
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) minIndex = j;
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0, k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    // Utility function to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    // Main to test all sorts
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original array:");
+        printArray(arr);
+
+        int[] arr1 = arr.clone();
+        bubbleSort(arr1);
+        System.out.print("Bubble Sort: ");
+        printArray(arr1);
+
+        int[] arr2 = arr.clone();
+        selectionSort(arr2);
+        System.out.print("Selection Sort: ");
+        printArray(arr2);
+
+        int[] arr3 = arr.clone();
+        insertionSort(arr3);
+        System.out.print("Insertion Sort: ");
+        printArray(arr3);
+
+        int[] arr4 = arr.clone();
+        mergeSort(arr4, 0, arr4.length - 1);
+        System.out.print("Merge Sort: ");
+        printArray(arr4);
+
+        int[] arr5 = arr.clone();
+        quickSort(arr5, 0, arr5.length - 1);
+        System.out.print("Quick Sort: ");
+        printArray(arr5);
+    }
+}
